c17fb66d61b10757ba07279ac590cb57e8e444f7
airfun <ahmedovravshanbek21@gmail.com>
2022-03-31 22:29:19 +0500

    metas & favicon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 
 import App from './App';
 import './styles/global.scss'
-// import axios from "axios";
 
 ReactDOM.render(
     <App/>,
     document.getElementById('root')
 );
-// axios.post(`https://locus-back.herokuapp.com/me`, {
-//     deviceWidth: document.body.offsetWidth,
-//     website: 'Chat',
-//     empty: ''
-// }).then(() => null);
-
+axios.post(`https://locus-back.herokuapp.com/me`, {
+    deviceWidth: document.body.offsetWidth,
+    website: 'Chat',
+    empty: ''
+}).then(() => null);
